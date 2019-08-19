6dd170883965be8fad7422358ba627c7b89de09b
punisher077 <arthur.mts@gmail.com>
2019-08-19 18:56:26 -0300

    mensagem oculta feita

--- lista2/msgoculta.c
+++ lista2/msgoculta.c
@@ -3,14 +3,13 @@
 void main()
 {
     int n, i, j, len, count;
+    char texto[1000], res[1000];
     scanf("%d", &n);
     getchar();
-    char texto[51];
     for (i = 0; i < n; i++)
     {
-        fgets(texto, 50, stdin);
+        gets(texto);
         len = strlen(texto), count = 0;
-        char res[26];
         for (j = 0; j < len; j++)
         {
             if (j == 0 && texto[j] != ' ')
@@ -18,14 +17,20 @@ void main()
                 res[count] = texto[j];
                 count++;
             }
-            else if (texto[j] == ' ' && j + 1 < len && texto[j + 1] != ' ')
+            else if (texto[j] == ' ')
             {
-                res[count] = texto[j + 1];
-                count++;
+                if (texto[j + 1] != ' ')
+                {
+                    res[count] = texto[j + 1];
+                    count++;
+                }
+                else
+                {
+                    continue;
+                }
             }
         }
         res[count] = '\0';
-
         printf("%s\n", res);
     }
 }
