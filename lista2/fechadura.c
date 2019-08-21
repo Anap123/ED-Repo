0ab76f2ea56b42df40f51fcb595a4664b0921f33
arthurmts <punisher077@github.com>
2019-08-21 10:25:02 -0300

    faltam 4 ._.

---
+++ lista2/fechadura.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  fechadura.c
+ *
+ *    Description:  Questao URI 2449
+ *
+ *        Version:  1.0
+ *        Created:  21/08/2019 10:13:43
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Arthur Mauricio 
+ *   Organization:  IFPB Campina Grande 
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+void main(){
+    int n, m;
+    scanf("%d %d", &n, &m);
+}
