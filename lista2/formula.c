325339cb39835b739477531b99dafe4abf2d8bbb
arthurmts <punisher077@github.com>
2019-08-22 13:22:00 -0300

    terminada formula

--- lista2/formula.c
+++ lista2/formula.c
@@ -37,21 +37,22 @@ void main(){
 
         int pontuacoes[p], maior, iPilotoC, in;
         for(i=0; i < s; i++){
-            for (j=0; j < p; j++) pontuacoes[j] = 0;
+            for(j=0; j < p; j++) pontuacoes[j] = 0;
             for(j=0; j < g; j++){
                 for(k = 0; k < p; k++){
                     iPilotoC = gps[j][k]-1;
                     pontuacoes[k] += tables[i][iPilotoC];
                 }
             }
-            maior, in = 0;
+            maior = in = 0;
+            
             for(j=0;j<p;j++) if(pontuacoes[j] >= maior) maior = pontuacoes[j];
-            for(j=0;j<p;j++){
-                if(pontuacoes[j] == maior){
-                    if(in)printf(" %d", j+1);
-                    else printf("%d", j+1), in = 1;
+            for(k=0;k<p;k++)
+                if(pontuacoes[k] == maior){
+                    if(!in) printf("%d", k+1), in=1;
+                    else printf(" %d",k+1);
                 }
-            }
+
             printf("\n");
         }
     }
