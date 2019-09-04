adaec834bedb194edd2ae008a4239de284938475
arthurmts <punisher077@github.com>
2019-09-04 18:10:08 -0300

    lista 3 completa!

--- lista3/questao4.c
+++ lista3/questao4.c
@@ -1,40 +1,42 @@
 #include <stdio.h>
+#include <math.h>
 //Fatorial recutsiva
-int fatorialRec(int n)
+double fatorialRec(int n)
 {
     if (n == 1)
-        return 1;
+        return 1.0;
     else
-        return n * fatorialRec(n - 1);
+        return (double) n * fatorialRec(n - 1.0);
 }
 
 //Fatorial iterativa
-int fatorialIt(int n)
+double fatorialIt(int n)
 {
-    int i, res = 1;
+    int i;
+    double res = 1.0;
     for (i = 2; i <= n; i++)
-        res *= i;
+        res = res * i;
     return res;
 }
 
 //Calculo do seno iterativo
-double senoIt(double x, int qt)
+long double senoIt(double x, int qt)
 {
-    int i, t = 1;
-    double res = 0;
-    for (i = 1; i <= qt; i++)
+    int i;
+    long double res = x, tmp;
+    for (i = 1; i < qt; i++)
     {
-        res = (i % 2 == 0) ? res + pow(x, t) / fatorialIt(t) : res - pow(x, t) / fatorialIt(t);
-        t += 2;
-    }
+        tmp = pow(x, 2*i+1) / fatorialIt((2*i)+1);
+        res = (i % 2) ? res - tmp : res + tmp;
+        }
     return res;
 }
 //Calculo do seno recursivamente
-double senoRec(double x, int qt)
+long double senoRec(double x, int qt)
 {
     int t = qt * 2 - 1;
     if (qt == 1)
-        return 1;
+        return x;
     else if (qt % 2 == 0)
         return senoRec(x, qt - 1) - (pow(x, t) / fatorialRec(t));
     else
@@ -43,11 +45,14 @@ double senoRec(double x, int qt)
 
 void main()
 {
-    printf("%d\n", fatorialRec(4));
+    // Testes das funções fatoriais
+    printf("Fatorial de 5 na recursiva: %lf\n", fatorialRec(5));
 
-    printf("%d\n", fatorialIt(4));
+    printf("Fatorial de 5 na iterativa: %lf\n", fatorialIt(5));
+    //Testes das funções seno
+    printf("Seno de 3.14 na iterativa: %Lf\n",  senoIt(3.14, 8));
 
-    printf("%lf\n", senoIt(3.14, 3));
+    printf("Seno de 3.14 na recursiva: %Lf\n", senoRec(3.14, 8));
 
-    printf("%lf\n", senoRec(3.14, 7));
+    printf("Seno de 3.14 no metodo sin(): %lf\n", sin(3.14));
 }
