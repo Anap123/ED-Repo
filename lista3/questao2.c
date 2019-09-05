bd7c57163b19c49862d020b603d93988626609c6
arthurmts <punisher077@github.com>
2019-09-05 16:00:06 -0300

    concertando q2

--- lista3/questao2.c
+++ lista3/questao2.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-int *strrchr2(char *str, char c)
+char *strrchr2(char *str, char c)
 {
-    int i, *loca = NULL;
+    int i;
+    char *loca =NULL;
     for (i = 0; str[i] != '\0'; i++) if (str[i] == c) loca = &str[i];
     return loca;
 }
 void main()
 {
-    int *pointer = strrchr2("aaaaas", 's');
+    char *pointer = strrchr2("aaaaas", 's');
+    printf("%p aponta para %c\n", pointer, *pointer);
+
+    pointer = strrchr2("aaaaas", 'a');
     printf("%p aponta para %c\n", pointer, *pointer);
 
     // Retorna nulo!
     pointer = strrchr2("aaas", 'c');
-    printf("%p", pointer);
+    printf("%p\n", pointer);
 }
